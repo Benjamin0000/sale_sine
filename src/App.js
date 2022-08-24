a3d4772cd1740713c797018cb1ffa505232f267d
gaurav3278 <gaurav.rai2020@vitbhopal.ac.in>
2022-08-24 09:58:12 +0530

    in progress

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/home/Home';
 import Calls from './pages/calls/Calls'
+import Deals from './pages/deals/Deals';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/calls' element={<Calls/>} />
+        <Route path='/deals' element={<Deals/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/deals/Deals.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Deals = () => {
+    return (
+        <>
+            <Navbar type='deals' />
+            <div className="dealsWrapper">
+                
+            </div>
+        </>
+    )
+}
+
+export default Deals
